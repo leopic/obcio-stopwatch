8172f1c5e3e6194adfa2560794a39e2b3231e265
Leo Picado <leopic@gmail.com>
2021-02-06 14:40:25 -0600

    Episode 180

--- ObcIO-Stopwatch/ContentView.swift
+++ ObcIO-Stopwatch/ContentView.swift
@@ -1,21 +1,46 @@
-//
-//  ContentView.swift
-//  ObcIO-Stopwatch
-//
-//  Created by Leo Picado on 2/6/21.
-//
-
 import SwiftUI
 
+struct RingStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    Circle()
+      .fill()
+      .overlay(
+        Circle()
+          .fill(Color.white)
+          .opacity(configuration.isPressed ? 0.3 : 0)
+      )
+      .overlay(
+        Circle()
+          .stroke(lineWidth: 2)
+          .foregroundColor(.white)
+          .padding(4)
+      )
+      .overlay(
+        configuration.label
+          .foregroundColor(.white)
+      )
+  }
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    HStack {
+      Button(action: {}) {
+        Text("Stop")
+      }
+      .foregroundColor(.red)
+      .frame(width: 75, height: 75, alignment: .center)
+      Button(action: {}) {
+        Text("Start")
+      }
+      .foregroundColor(.green)
+      .frame(width: 75, height: 75, alignment: .center)
+    }.buttonStyle(RingStyle())
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
