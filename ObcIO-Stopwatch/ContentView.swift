54e436275c32ff09fd4b4a60e6f0e771e8f57ee3
Leo Picado <leopic@gmail.com>
2021-02-06 14:08:05 -0600

    Initial Commit

---
+++ ObcIO-Stopwatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObcIO-Stopwatch
+//
+//  Created by Leo Picado on 2/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObcIO-Stopwatch/ObcIO_StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObcIO_StopwatchApp.swift
+//  ObcIO-Stopwatch
+//
+//  Created by Leo Picado on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ObcIO_StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
