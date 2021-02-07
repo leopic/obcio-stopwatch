fa9a7e19c079b954176ad8e4a4cbf6d0bdb149fa
Leo Picado <leopic@gmail.com>
2021-02-06 23:08:45 -0600

    Episode 181

--- ObcIO-Stopwatch/ContentView.swift
+++ ObcIO-Stopwatch/ContentView.swift
@@ -1,13 +1,24 @@
 import SwiftUI
 
-struct RingStyle: ButtonStyle {
-  func makeBody(configuration: Configuration) -> some View {
-    Circle()
+struct SizeKey: PreferenceKey {
+  static let defaultValue: [CGSize] = []
+  static func reduce(value: inout [CGSize], nextValue: () -> [CGSize]) {
+    value.append(contentsOf: nextValue())
+  }
+}
+
+struct ButtonCircle: ViewModifier {
+  @State var size: CGSize? = nil
+
+  let isPressed: Bool
+
+  func body(content: Content) -> some View {
+    let background = Circle()
       .fill()
       .overlay(
         Circle()
           .fill(Color.white)
-          .opacity(configuration.isPressed ? 0.3 : 0)
+          .opacity(isPressed ? 0.3 : 0)
       )
       .overlay(
         Circle()
@@ -15,10 +26,68 @@ struct RingStyle: ButtonStyle {
           .foregroundColor(.white)
           .padding(4)
       )
-      .overlay(
-        configuration.label
-          .foregroundColor(.white)
-      )
+
+    let foreground =
+      content
+      .fixedSize()
+      .padding(15)
+      .equalSize()
+      .foregroundColor(.white)
+
+    return foreground
+      .background(background)
+  }
+}
+
+struct SizeEnvironmentKey: EnvironmentKey {
+  static var defaultValue: CGSize? = nil
+}
+
+extension EnvironmentValues {
+  var size: CGSize? {
+    get {
+      self[SizeEnvironmentKey.self]
+    }
+    set {
+      self[SizeEnvironmentKey.self] = newValue
+    }
+  }
+}
+
+fileprivate struct EqualSize: ViewModifier {
+  @Environment(\.size) private var size
+
+  func body(content: Content) -> some View {
+    content.overlay(GeometryReader { proxy in
+      Color.clear.preference(key: SizeKey.self, value: [proxy.size])
+    })
+    .frame(width: size?.width, height: size?.width)
+  }
+}
+
+fileprivate struct EqualSizes: ViewModifier {
+  @State var width: CGFloat?
+
+  func body(content: Content) -> some View {
+    content.onPreferenceChange(SizeKey.self, perform: { sizes in
+      self.width = sizes.map { $0.width }.max()
+    }).environment(\.size, width.map { CGSize(width: $0, height: $0) })
+  }
+}
+
+extension View {
+  func equalSize() -> some View {
+    self.modifier(EqualSize())
+  }
+
+  func equalSizes() -> some View {
+    self.modifier(EqualSizes())
+  }
+}
+
+struct RingStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label.modifier(ButtonCircle(isPressed: configuration.isPressed))
   }
 }
 
@@ -26,16 +95,17 @@ struct ContentView: View {
   var body: some View {
     HStack {
       Button(action: {}) {
-        Text("Stop")
+        Text("Detener")
       }
       .foregroundColor(.red)
-      .frame(width: 75, height: 75, alignment: .center)
+
       Button(action: {}) {
-        Text("Start")
+        Text("GO")
       }
       .foregroundColor(.green)
-      .frame(width: 75, height: 75, alignment: .center)
-    }.buttonStyle(RingStyle())
+    }
+    .equalSizes()
+    .buttonStyle(RingStyle())
   }
 }
 
